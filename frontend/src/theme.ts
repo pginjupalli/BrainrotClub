4a4001bc4a2632939cf15bff824ce398003c04e9
vincenzo-sorcigli <57498723+vincenzo-sorcigli@users.noreply.github.com>
2025-02-22 15:12:30 -0500

    update theme

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -2,6 +2,18 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
+    palette: {
+        type: "light",
+        primary: {
+            main: "#9e4a6f",
+        },
+        secondary: {
+            main: "#0090f5",
+        },
+        error: {
+            main: "#e20e11",
+        },
+    },
     typography: {
         fontFamily: "var(--font-roboto)",
     },
